651a0f353795f328304c3c565d8778f11b0a51e4
NakYoun Keum <goldnyou@gmail.com>
2017-09-26 09:46:04 +0900

    added output 'Tell his world' exit quit exit
    q
    /?
    q

--- helloGit.java
+++ helloGit.java
@@ -2,5 +2,6 @@ public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello Git");
 		System.out.println("Tell your world");
+		System.out.println("Tell his world");
 	}
 }
